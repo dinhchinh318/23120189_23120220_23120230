26c0b1bd9a11f546fbf75124de4dbca7a942059e
dinhchinh318 <chinh3182005@gmail.com>
2025-04-13 14:34:24 +0000

    Them folder source va release

---
+++ source/models/DienThoai.h
@@ -0,0 +1,20 @@
+#ifndef _DIEN_THOAI_H_
+#define _DIEN_THOAI_H_
+
+#include "../lib/lib.cpp"
+
+
+class DienThoai 
+{
+    private:
+        std::string maSP, tenSP, hang;
+        double gia;
+    public:
+        virtual void nhap();
+        virtual void xuat();
+        double getGia() const;
+        std::string getMaSP() const;
+        virtual ~DienThoai() {}
+    };
+
+#endif
